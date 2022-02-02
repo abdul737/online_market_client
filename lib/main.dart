805dfda686f18c0d2e9bc743964ea9b3b8237654
Abdulbosid Khamidov <abdulbosid.kh@gmail.com>
2022-02-02 13:03:30 +0500

    Initial design template

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:online_market_client/pages/login.dart';
+import 'package:online_market_client/routes.dart';
+import 'package:online_market_client/screens/profile/profile_screen.dart';
+import 'package:online_market_client/screens/splash/splash_screen.dart';
+import 'package:online_market_client/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
+      theme: theme(),
+      // home: SplashScreen(),
+      // We use routeName so that we dont need to remember the name
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
     );
   }
 }
