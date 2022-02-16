2a67f69722ee45c1eab5c46ab4992cdb002574a8
Ismoil <muhammadjonov.1996i@gmail.com>
2022-02-16 13:20:41 +0500

    ditails ekranidag wijit togirlandi

---
+++ lib/components/if_description.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:online_market_client/models/Product.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../size_config.dart';
+
+class FavoriteWidget extends StatefulWidget {
+  final bool isFavourite;
+
+  const FavoriteWidget({
+    Key? key,
+    required this.isFavourite,
+  }) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool isFavourite2 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(getProportionateScreenWidth(2)),
+      width: getProportionateScreenWidth(40),
+      decoration: BoxDecoration(
+        color: isFavourite2 ? const Color(0xFFFFE6E6) : const Color(0xFFF5F6F9),
+        shape: BoxShape.circle,
+      ),
+      child: TextButton(
+        onPressed: _toggleFavorite,
+        child: SvgPicture.asset(
+          "assets/icons/Heart Icon_2.svg",
+          color:
+              isFavourite2 ? const Color(0xFFFF4848) : const Color(0xFFDBDEE4),
+          height: getProportionateScreenWidth(20),
+        ),
+      ),
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (isFavourite2) {
+        isFavourite2 = false;
+      } else {
+        isFavourite2 = true;
+      }
+    });
+  }
+}
