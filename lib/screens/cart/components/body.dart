9b41ce1225ba95f3a6161507274bc48a944b6ceb
Ismoil96 <muhammadjonov.1996i@gmail.com>
2022-02-28 10:56:59 +0500

    tekshirvorarsan

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:online_market_client/models/Cart.dart';
+import 'package:online_market_client/screens/details/details_screen.dart';
 
 import '../../../size_config.dart';
 import 'cart_card.dart';
@@ -19,34 +22,42 @@ class _BodyState extends State<Body> {
       padding:
           EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
       child: ListView.builder(
-        itemCount: demoCarts.length,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Dismissible(
-            key: Key(demoCarts[index].product.id.toString()),
-            direction: DismissDirection.endToStart,
-            onDismissed: (direction) {
-              setState(() {
-                demoCarts.removeAt(index);
-              });
-            },
-            background: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Color(0xFFFFE6E6),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                children: [
-                  Spacer(),
-                  SvgPicture.asset("assets/icons/Trash.svg"),
-                ],
-              ),
-            ),
-            child: CartCard(cart: demoCarts[index]),
-          ),
-        ),
-      ),
+          itemCount: demoCarts.length,
+          itemBuilder: (context, index) => GestureDetector(
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  DetailsScreen.routeName,
+                  arguments: ProductDetailsArguments(
+                    product: demoCarts[index].product,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Dismissible(
+                    key: Key(demoCarts[index].product.id.toString()),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) {
+                      setState(() {
+                        demoCarts.removeAt(index);
+                      });
+                    },
+                    background: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFFE6E6),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Row(
+                        children: [
+                          const Spacer(),
+                          SvgPicture.asset("assets/icons/Trash.svg"),
+                        ],
+                      ),
+                    ),
+                    child: CartCard(cart: demoCarts[index]),
+                  ),
+                ),
+              )),
     );
   }
 }
