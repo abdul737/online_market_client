08824b519a8b38869b2988d61738b7fd54589f81
Ismoil96 <muhammadjonov.1996i@gmail.com>
2022-04-11 17:15:57 +0500

    36 reklama banner

--- lib/screens/home/components/carousel_bar.dart
+++ lib/screens/home/components/carousel_bar.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:online_market_client/components/banner_widget.dart';
+import 'package:online_market_client/constants.dart';
+import 'package:online_market_client/screens/home/components/discount_banner.dart';
 
 class CarouselBar extends StatelessWidget {
   const CarouselBar({Key? key}) : super(key: key);
@@ -12,9 +13,11 @@ class CarouselBar extends StatelessWidget {
           height: 90,
           enlargeCenterPage: true,
           viewportFraction: 1,
-          autoPlayInterval: const Duration(seconds: 2),
+          autoPlayInterval: const Duration(seconds: carouselAutoplayDuration),
           autoPlay: true,
         ),
-        items: banner.toList());
+        items: [
+          const DiscountBanner(),
+        ].toList());
   }
 }
